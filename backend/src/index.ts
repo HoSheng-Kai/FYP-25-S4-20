2722e67ca91616c8b09724a153a077025b02b4a3
Ho Sheng-Kai <hoshengkai1308@gmail.com>
2025-12-12 13:41:33 +0800

    added cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 
 import userRouter from './router/userRouter';
 import distributorRouter from './router/distributorRouter';
@@ -8,6 +9,8 @@ import productRouter from './router/productRouter';
 import notificationRouter from './router/notificationRouter';
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/users', userRouter);
