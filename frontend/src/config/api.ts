c1d1d4709d12687d2d0f953fc15a43f139e15298
Ho Sheng-Kai <hoshengkai1308@gmail.com>
2025-12-30 21:25:35 +0800

    removed frontend

--- frontend/src/config/api.ts
+++
@@ -1,8 +0,0 @@
-// Frontend API config
-
-export const API_ROOT = "http://localhost:3000/api";
-
-export const USERS_API_BASE_URL = `${API_ROOT}/users`;
-export const PRODUCTS_API_BASE_URL = `${API_ROOT}/products`;
-export const NOTIFICATIONS_API_BASE_URL = `${API_ROOT}/notifications`;
-export const DISTRIBUTOR_API_BASE_URL = `${API_ROOT}/distributor`;
